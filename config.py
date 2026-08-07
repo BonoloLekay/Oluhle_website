5020d992ca13d9db91a3e04578a38de62daf4ebe
Bonolo Lekalakala <mmabosigo058@gmail.com>
2026-08-07 19:00:39 +0200

    Add SECRET_KEY configuration for production

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from flask_mail import Mail
 
 load_dotenv()
 
-mail=Mail()
+mail = Mail()
 
 class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
 
-     # Email Configuration
-    MAIL_SERVER = os.getenv("MAIL_SERVER")
-    MAIL_PORT = int(os.getenv("MAIL_PORT"))
+    MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp.gmail.com")
+    MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
     MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True").lower() == "true"
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
-    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER")
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME", "")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", "")
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER", "")
