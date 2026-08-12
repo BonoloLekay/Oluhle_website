f2157eb5f2b9c2b17fd4744b6605383bd0cd63ec
Bonolo Lekalakala <mmabosigo058@gmail.com>
2026-08-12 17:20:08 +0200

    Fix sitemap XML

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, Response
 from config import Config, mail
 
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
@@ -12,16 +13,14 @@ def create_app():
 
     app.register_blueprint(home_bp)
     app.register_blueprint(booking_bp)
-    
+
     @app.route("/sitemap.xml")
     def sitemap():
         xml = """<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-
     <url>
         <loc>https://oluhleresorts.co.za/</loc>
     </url>
-
 </urlset>
 """
         return Response(xml, mimetype="application/xml")
