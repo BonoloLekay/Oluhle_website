ee9069388f43fa5b14048393a5b2655d91a55311
Bonolo Lekalakala <mmabosigo058@gmail.com>
2026-08-12 17:32:40 +0200

    Use canonical www domain in sitemap

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,7 @@ def create_app():
         xml = """<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
-        <loc>https://oluhleresorts.co.za/</loc>
+        <loc>https://www.oluhleresorts.co.za/</loc>
     </url>
 </urlset>
 """
